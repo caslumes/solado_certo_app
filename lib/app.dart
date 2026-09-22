bf92835fa8b61483a7a7f772987f6602489d428e
caslumes <caslumes@outlook.com>
2026-09-22 19:12:08 -0300

    feat: add splash page skeleton

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:solado_certo_app/features/splash/presentation/pages/splash_page.dart';
 
 class App extends StatelessWidget {
-  const App
-({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
   }
 }
